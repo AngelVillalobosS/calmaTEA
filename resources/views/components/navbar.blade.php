97ca914b6f7cbaafd0312f830c15051a7f1f4de5
AngelVillaloboS <angel.villalobos2269@uppuebla.edu.mx>
2025-03-23 15:13:13 -0600

    navbar 50%

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,13 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/navbar.css')}}">
 </head>
+
 <body>
-    <div class="navbar">
-        <h4>calmaTEA</h4>
-        
+    <div class="container">
+        <nav class="navbar">
+            <img src="{{asset('images/assets/calmatea_icon.png')}}" alt="logo de un corazón color aqua junto a un circulo" class="iconnvbar">
+            <div class="titlenvbr">
+                <a href="#" id="title">CalmaTea</a>
+                <p id="slogan">"Equilibra tus emociones, encuentra tu calma"</p>
+            </div>
+        </nav>
     </div>
 </body>
+
 </html>
